7c1005a5c5728f114d932a1ee57455c6581e80c7
Budo84 <lamanteafabio@gmail.com>
2026-01-15 10:21:50 +0100

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import HomeLayout from './HomeLayout.vue'
 
+import './style.css'
+
 export default {
   extends: DefaultTheme,
   // Sovrascrive il layout solo quando serve (es. nella Home)
